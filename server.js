69d8787774a902d25c4acaf35f5419821ae82a5e
Andrew <agbales@gmail.com>
2016-06-30 21:58:12 +0000

    removed passport

--- server.js
+++ server.js
@@ -7,27 +7,7 @@ var passport = require('passport');
 var session = require('express-session');
 var moment = require('moment');
 
-
 var app = express();
-// require('dotenv').load();
-// require('./app/config/passport')(passport);
-
-// mongoose.connect(process.env.MONGO_URI);
-
-// app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-// app.use('/public', express.static(process.cwd() + '/public'));
-// app.use('/common', express.static(process.cwd() + '/app/common'));
-
-// app.use(session({
-// 	secret: 'secretClementine',
-// 	resave: false,
-// 	saveUninitialized: true
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
