a2958ed628b6272d4d894dd37c09ad2af0bbe2b3
Andrew <agbales@gmail.com>
2016-06-30 20:53:23 +0000

    timestamp initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var moment = require('moment');
+
+
+var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+	process.on('uncaughtException', function (err) {
+	    console.log(err);
+	}); 
+});
+
+app.get('/:query', function(req, res) {
+	var date = req.params.query;
+	var u,
+		n
+	
+	// If unix, else make unix & format text
+	if (date == parseInt(date)) {
+		u = parseInt(date)
+		n = moment.unix(date).format('MMMM DD, YYYY')
+	} else {
+		u = moment(date).unix()
+		n = moment(date).format('MMMM DD, YYYY')
+	}
+	
+	// If Unix not integer, failure. Return null.
+	if (u !== parseInt(u)) {
+		u = null;
+		n = null;
+	}
+	
+	var obj = { 
+		unix : u,
+		natural : n
+	};
+	
+	console.log(JSON.stringify(obj));
+	res.send(JSON.stringify(obj));
+});
